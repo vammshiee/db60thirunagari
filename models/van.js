37a4bc838c14b90900f810ec7677b40da38d2083
vammshiee <84035025+vammshiee@users.noreply.github.com>
2021-11-10 20:40:37 -0600

    code changes

--- models/van.js
+++ models/van.js
@@ -2,18 +2,14 @@ const mongoose = require("mongoose")
 const vanSchema = mongoose.Schema({
     Name: {
         type: String,
-        required: [true, "Name of van can not be blank"]
+        
     },
     Company: {
         type: String,
-        required: [true, "van company can not be blank"]
+       
     },
     Price: {
         type: Number,
-        required: [true, "Price of the van is required"],
-        min: [1000, "van price Should be minimum of $1000 "],
-        max: [500000, "van price Cannot be greater than $500000 "]
-
-    }
+            }
 })
 module.exports = mongoose.model("Van", vanSchema)
