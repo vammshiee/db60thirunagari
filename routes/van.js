e04266c289f89e40293a739d9b743d3e8e93fd7b
vammshiee <84035025+vammshiee@users.noreply.github.com>
2021-11-17 10:09:39 -0600

    added changes

--- routes/van.js
+++ routes/van.js
@@ -2,6 +2,12 @@ var express = require('express');
 const van_controlers= require('../controllers/van');
 var router = express.Router();
 
-/* GET costumes */
+/* GET Vans */
 router.get('/', van_controlers.van_view_all_Page );
+
+/* GET detail van page */
+router.get('/detail', van_controlers.van_view_one_Page);
+
 module.exports = router;
+
+
