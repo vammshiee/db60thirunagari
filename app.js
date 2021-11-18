bc72d9cf023264581c5196c7ddc90c71bc17fdee
vammshiee <84035025+vammshiee@users.noreply.github.com>
2021-11-18 13:28:26 -0600

    added account

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var van = require("./models/van");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }
+  app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+   }));
+   app.use(passport.initialize());
+   app.use(passport.session());
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -75,7 +97,13 @@ app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 // app.use('/van/create',vancreateRouter);
-
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account'));
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
